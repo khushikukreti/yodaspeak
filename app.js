9aa5c013e2233694b864efe28f87f6e14e2dace0
khushi <khushikukreti20@gmail.com>
2021-01-17 22:09:30 +0530

    added error handler function

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ var inputText=document.querySelector("#inputText");
 var outputText=document.querySelector("#output");
 
 var serverURL="https://api.funtranslations.com/translate/yoda.json"
-function getTranslationURL(input){
-    return serverURL+ "?" + "text=" + input
+
+function getTranslationURL(inputURL) {
+    return serverURL + "?" + "text=" + inputURL
+}
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
 }
 
-function clickHandler(){
-    var inputT=inputText.value;
+
+function clickHandler() {
+    var inputT = inputText.value; // taking input
+
+    // calling server for processing
     fetch(getTranslationURL(inputT))
-    .then(Response=>Response.json())
-    .then(json=>{
-        var translateText=json.contents.translated;
-        outputText.innerText=translateText;
-    })
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputText.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
 };
 btnTranslate.addEventListener("click", clickHandler)
